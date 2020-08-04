c6b4861fba3ba920ffbfc7ce607fde77207cb5a8
manemejeff <kmb14056@yandex.ru>
2020-08-04 10:17:08 +0400

    callback functions

---
+++ Functions/callback.js
@@ -0,0 +1,23 @@
+function first() {
+    setTimeout( function(){
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log('I am lerning ' + lang);
+    callback();
+}
+
+function done() {
+    console.log('Yay')
+}
+
+learnJS('JavaScript', done);
