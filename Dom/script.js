a1d693677c433db769f53e60c7736e9768accd32
manemejeff <kmb14056@yandex.ru>
2020-08-05 17:39:06 +0400

    dom element actions

--- Dom/script.js
+++ Dom/script.js
@@ -4,9 +4,38 @@ let box = document.getElementById('box'),
     heart = document.querySelectorAll('.heart'),
     oneHeart = document.querySelector('.heart');
 
-console.log(box);
-console.log(btn);
-console.log(btn[0]);
-console.log(circle[2]);
-console.log(heart);
-console.log(oneHeart);
+// console.log(box);
+// console.log(btn);
+// console.log(btn[0]);
+// console.log(circle[2]);
+// console.log(heart);
+// console.log(oneHeart);
+
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '100%';
+
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+
+// heart.forEach(function (item, i, hearts) {
+//     item.style.backgroundColor = 'blue';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Just text');
+
+div.classList.add('black');
+
+div.innerHTML = '<h1>Hello World</h1>';
+div.textContent = 'Hello World!';
+
+// document.body.appendChild(div);
+
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+document.body.replaceChild(btn[1], circle[1]);
