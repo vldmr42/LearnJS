3e92f6ebecad40d7f53aa160d44a3a2f3a091db3
manemejeff <kmb14056@yandex.ru>
2020-08-05 10:23:03 +0400

    dom

---
+++ Dom/script.js
@@ -0,0 +1,12 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart');
+
+console.log(box);
+console.log(btn);
+console.log(btn[0]);
+console.log(circle[2]);
+console.log(heart);
+console.log(oneHeart);
