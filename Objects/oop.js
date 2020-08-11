216ae15d2e979c9a150b04ae4703d2451c3482e5
manemejeff <kmb14056@yandex.ru>
2020-08-11 09:14:38 +0400

    function and class

--- Objects/oop.js
+++ Objects/oop.js
@@ -9,5 +9,38 @@ let john = {
 
 john.__proto__ = soldier;
 
-console.log(john);
-console.log(john.armor  )
+// console.log(john);
+// console.log(john.armor  )
+
+//  Old functions
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log('Hello ' + this.name)
+//     };
+// }
+
+// New classes
+'use strict';
+class User {
+    constructor(name, id) {
+        this.id = id;
+        this.name = name;
+        this.human = true;
+    }
+    hello() {
+        console.log('Hello ${this.name}');
+    }
+    exit() {
+        console.log('User ${this.name} has left');
+    }
+}
+
+let ivan = new User('Ivan', 25),
+    alex = new User('Alex', 20);
+console.log(ivan);
+console.log(alex);
+alex.hello();
+ivan.exit();
