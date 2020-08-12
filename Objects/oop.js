3d5fe01a28c7b00674bb92adacd282a2b0616950
manemejeff <kmb14056@yandex.ru>
2020-08-12 11:09:56 +0400

    call context

--- Objects/oop.js
+++ Objects/oop.js
@@ -23,24 +23,84 @@ john.__proto__ = soldier;
 // }
 
 // New classes
-'use strict';
-class User {
-    constructor(name, id) {
-        this.id = id;
-        this.name = name;
-        this.human = true;
-    }
-    hello() {
-        console.log('Hello ${this.name}');
-    }
-    exit() {
-        console.log('User ${this.name} has left');
-    }
-}
+// class User {
+//     constructor(name, id) {
+//         this.id = id;
+//         this.name = name;
+//         this.human = true;
+//     }
+//     hello() {
+//         console.log('Hello ' + this.name);
+//     }
+//     exit() {
+//         console.log('User ' + this.name + ' has left');
+//     }
+// }
 
-let ivan = new User('Ivan', 25),
-    alex = new User('Alex', 20);
-console.log(ivan);
-console.log(alex);
-alex.hello();
-ivan.exit();
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 20);
+// console.log(ivan);
+// console.log(alex);
+// alex.hello();
+// ivan.exit();
+
+// 1) this - window/undefined
+
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+// }
+// showThis(4, 5);
+
+// 2) this - object
+
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function () {
+//         console.log(this);
+
+//         function shout() {
+//             // this - window
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+// let user = {
+//     name: 'John',
+// };
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + ' ' + surname);
+// }
+
+// console.log(sayName.call(user, 'Smith'));
+// console.log(sayName.apply(user, ['Snow']));
+
+
+// function count(number) {
+//     return this * number;
+// }
+
+// let double = count.bind(2);
+// console.log(double(3));
+// console.log(double(5));
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+
+    function showThis() {
+        console.log(this);
+    }
+    showThis();
+});
