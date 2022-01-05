4a328bad0dfe88da3ce9a7255a86050f8d07e3ba
manemejeff <kmb14056@yandex.ru>
2022-01-05 13:05:10 +0400

    datatypes and user interaction

---
+++ Basics/Data types/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Numerical types
+let number = 4.6;
+console.log(-4/0);
+console.log('string' * 9);
+
+// Type string
+const person = 'Alex';
+
+// Undefined type
+let und;
+console.log(und);
+
+// Object type
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+console.log(obj['name']);
+
+// Array obj
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
---
+++ Basics/User interaction/js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Simple message popup
+// alert('Hello');
+
+// True/False menu
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// Type answer menu
+// const answer = prompt("How old are you?", "18");
+// console.log(answer);
+
+const answer = [];
+
+answer[0] = prompt('What is your name?', '');
+answer[1] = prompt('What is your last name?', '');
+answer[2] = prompt('How old are you?', '');
+
+document,write(answer);
