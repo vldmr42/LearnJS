98075aad31f0519620598e9e3d1601170ef5e4c4
manemejeff <kmb14056@yandex.ru>
2022-01-07 15:03:53 +0400

    str and num methods

---
+++ Basics/Num and str methods/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 3, 4];
+
+console.log(str.toUpperCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Slice Me';
+console.log(logg.slice(6));
+
+console.log(logg.substring(6));
+
+console.log(logg.substr(6, 6));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
