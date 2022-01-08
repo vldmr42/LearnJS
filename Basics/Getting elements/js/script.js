ec8db5b6d9a328d5ec873ecbd72372b7938e5542
manemejeff <kmb14056@yandex.ru>
2022-01-08 23:05:44 +0400

    getting elements, query selectors

---
+++ Basics/Getting elements/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const box = document.getElementById('box');
+// console.log(box);
+
+const buttons = document.getElementsByTagName('button');
+// console.log(buttons[1]);
+
+const circles = document.getElementsByClassName('circle');
+// console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
