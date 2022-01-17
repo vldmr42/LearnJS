7bc6aee5696c17c7edbb3c18aee14aeeb0788e48
manemejeff <kmb14056@yandex.ru>
2022-01-17 21:04:51 +0400

    date obj

---
+++ InWork/Dates/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const now = new Date();
+
+// console.log(now.getMonth());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+
+console.log(now.setHours(18));
+console.log(now);
