58dab180f348cfb4f79a3c33a83769fd3ba8d9b7
manemejeff <kmb14056@yandex.ru>
2022-01-27 20:25:44 +0400

    fetch

---
+++ Advanced/Fetch/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({name: 'Alex'}),
+    headers:{
+        'Content-type': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(json => console.log(json));
