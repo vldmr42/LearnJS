83bfe066dea0f22a3327ec1cdb885e190ce3446b
Колесников Владимир Валерьевич <18635683@sigma.sbrf.ru>
2022-02-09 10:13:14 +0400

    functions and objects

---
+++ TS/TS Complete Guide/Annotations with functions and objects/functions.ts
@@ -0,0 +1,51 @@
+const add = (a: number, b: number): number => {
+    return a+b;
+};
+
+const subtract = (a: number, b:number): number => {
+    return a - b;
+}
+
+function divide(a: number, b: number): number {
+    return a/ b;
+};
+
+const multiply = function(a: number, b: number): number {
+    return a * b;
+};
+
+const logger = (message: string): void => {
+    console.log(message);
+};
+
+const throwError = (message: string): never => {
+    throw new Error(message);
+};
+
+const forecast = {
+    date: new Date(),
+    weather: 'sunny',
+};
+
+// DESTRUCTURING
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+    console.log(date);  
+    console.log(weather);
+};
+
+// ANNOTATIONS AROUND OBJECTS
+const profile = {
+    name: 'alex',
+    age: 20,
+    coords: {
+        lat: 0,
+        lng: 15
+    },
+
+    setAge(age: number): void {
+        this.age = age;
+    }
+};
+
+const {age}: {age: number} = profile;
+const {coords: {lat, lng}}: {coords: {lat: number, lng: number}} = profile;
