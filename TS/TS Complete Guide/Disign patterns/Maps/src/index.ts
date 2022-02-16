4ae9264bf5df8f590df6369d1784d0216a1d1d64
18635683 <mail@mail.com>
2022-02-16 14:40:09 +0400

    Company User and CustomMap classes

--- TS/TS Complete Guide/Disign patterns/Maps/src/index.ts
+++ TS/TS Complete Guide/Disign patterns/Maps/src/index.ts
@@ -1,7 +1,6 @@
-import {User} from "./User";
-import {Company} from "./Company";
+/// <reference types="@types/google.maps" />
+// import {User} from "./User";
+// import {Company} from "./Company";
+import { CustomMap } from "./CustomMap";
 
-const  user = new User();
-const company = new Company();
-
-
+const customMap = new CustomMap("map");
