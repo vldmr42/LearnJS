402ae9b071e3be9d72438e412d56f6ba4f9a5d27
18635683 <mail@mail.com>
2022-02-16 16:20:17 +0400

    wrapup

--- TS/TS Complete Guide/Disign patterns/Maps/src/Company.ts
+++ TS/TS Complete Guide/Disign patterns/Maps/src/Company.ts
@@ -1,12 +1,14 @@
-import * as faker from "faker";
+import * as faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'blue';
 
   constructor() {
     this.companyName = faker.company.companyName();
