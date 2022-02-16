2c4a65c5a56efa5b5dbea8bf4fc9fce2ee4e8033
18635683 <mail@mail.com>
2022-02-16 14:58:15 +0400

    addCompanyMarker

--- TS/TS Complete Guide/Disign patterns/Maps/src/index.ts
+++ TS/TS Complete Guide/Disign patterns/Maps/src/index.ts
@@ -1,8 +1,11 @@
 /// <reference types="@types/google.maps" />
-// import {User} from "./User";
-// import {Company} from "./Company";
+import { User } from "./User";
+import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
-console.log("123 123 123");
-
+const user = new User();
+const company = new Company();
 const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
