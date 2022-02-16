65e99b9526287e57507110ae150316995cd21243
18635683 <mail@mail.com>
2022-02-16 16:19:18 +0400

    popup windows

--- TS/TS Complete Guide/Disign patterns/Maps/src/Company.ts
+++ TS/TS Complete Guide/Disign patterns/Maps/src/Company.ts
@@ -1,20 +1,28 @@
 import * as faker from "faker";
 
 export class Company {
-    companyName: string;
-    catchPhrase: string;
-    location: {
-        lat: number;
-        lng: number;
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
     };
+  }
 
-    constructor() {
-        this.companyName = faker.company.companyName();
-        this.catchPhrase = faker.company.catchPhrase();
-        this.location = {
-            lat: parseFloat(faker.address.latitude()),
-            lng: parseFloat(faker.address.longitude())
-        }
-    }
-
+  markerContent(): string {
+    return `
+        <div>
+            <h1>Company Name: ${this.companyName}</h1>
+            <h3>CatchPhrase: ${this.catchPhrase}</h3>
+        </div>
+        `;
+  }
 }
--- TS/TS Complete Guide/Disign patterns/Maps/src/User.ts
+++ TS/TS Complete Guide/Disign patterns/Maps/src/User.ts
@@ -1,17 +1,21 @@
-import * as faker from 'faker';
+import * as faker from "faker";
 
 export class User {
-    name: string;
-    location: {
-        lat: number;
-        lng: number;
-    }
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
 
-    constructor() {
-        this.name = faker.name.firstName();
-        this.location = {
-            lat: parseFloat(faker.address.latitude()),
-            lng: parseFloat(faker.address.longitude())
-        };
-    }
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `User Name  ${this.name}`;
+  }
 }
