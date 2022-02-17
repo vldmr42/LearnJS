c9ab7c456f55b7ac1e8413c08690a82c57527af8
18635683 <mail@mail.com>
2022-02-17 12:33:09 +0400

    Collections and LinkedList implementation

--- TS/TS Complete Guide/More On Design Patterns/sort/src/index.ts
+++ TS/TS Complete Guide/More On Design Patterns/sort/src/index.ts
@@ -1,7 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const collectionToSort = new NumbersCollection([10, 3, -5, 0]);
+// const collectionToSort = new CharactersCollection('XaaabrtyErt');
+const collectionToSort = new LinkedList();
+collectionToSort.add(500);
+collectionToSort.add(23);
+collectionToSort.add(34);
+collectionToSort.add(0);
+collectionToSort.add(1000);
+const sorter = new Sorter(collectionToSort);
 sorter.sort();
 console.log(sorter.collection);
+collectionToSort.print();
