7dc670880146b38909b3e7c92f76fe1c9884dac0
vldmr42 <kmb14056@yandex.ru>
2022-02-24 17:59:41 +0400

    Random age and form rerender

--- TS/TS Complete Guide/Web/src/views/UserForm.ts
+++ TS/TS Complete Guide/Web/src/views/UserForm.ts
@@ -1,22 +1,36 @@
+import { isNull } from 'util';
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element | null, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onButtonClick(): void {
-    console.log('Hi there');
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click Me</button>
+        <button class="set-age">Set Random Age</button>
       </div>
     `;
   }
@@ -34,11 +48,15 @@ export class UserForm {
   }
 
   render(): void {
+    if (this.parent) this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
 
-    this.parent.append(templateElement.content);
+    if (this.parent) {
+      this.parent.append(templateElement.content);
+    }
   }
 }
