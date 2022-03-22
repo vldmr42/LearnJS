9c1c436335e2b56055343309a268e53aebf19675
manemejeff <kmb14056@yandex.ru>
2022-03-22 19:56:57 +0400

    login routes

---
+++ TS/TS Complete Guide/Express/src/index.ts
@@ -0,0 +1,14 @@
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['laskdjf'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
---
+++ TS/TS Complete Guide/Express/src/routes/loginRoutes.ts
@@ -0,0 +1,54 @@
+import cookieSession from 'cookie-session';
+import { Router, Response, Request } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+  <form method="POST">
+    <div>
+      <label>Email</label>
+      <input name="email" />
+    </div>
+    <div>
+      <label>Password</label>
+      <input name="password" type="password" />
+    </div>
+    <button>Submit</button>
+  </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (email && password && email === 'hi@hi.com' && password === 'password') {
+    // mark person as loggedin
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <div>You are logged in</div>
+      <a href="/logout">Logout</a>
+    </div>
+    `);
+  } else {
+    res.send(`
+    <div>
+      <div>You are not logged in</div>
+      <a href="/login">Login</a>
+    </div>
+    `);
+  }
+});
+
+export { router };
