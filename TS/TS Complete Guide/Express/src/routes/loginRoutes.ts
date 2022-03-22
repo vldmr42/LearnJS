6a4137eae6214310cbd1cd4a7e84b7308aecf487
manemejeff <kmb14056@yandex.ru>
2022-03-22 20:13:57 +0400

    logout and protected routes

--- TS/TS Complete Guide/Express/src/routes/loginRoutes.ts
+++ TS/TS Complete Guide/Express/src/routes/loginRoutes.ts
@@ -1,8 +1,18 @@
 import cookieSession from 'cookie-session';
-import { Router, Response, Request } from 'express';
+import { Router, Response, Request, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
 }
 
 const router = Router();
@@ -27,7 +37,10 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
   if (email && password && email === 'hi@hi.com' && password === 'password') {
-    // mark person as loggedin
+    if (req.session) {
+      req.session.loggedIn = true;
+      res.redirect('/protected');
+    }
   } else {
     res.send('Invalid email or password');
   }
@@ -49,6 +62,15 @@ router.get('/', (req: Request, res: Response) => {
     </div>
     `);
   }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user');
 });
 
 export { router };
